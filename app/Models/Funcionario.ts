6e0100a856ea83e82d37647f97b88dfd4a061ce3
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-09-18 16:14:20 -0300

    Migration e Model da tabela funcionarios.

---
+++ app/Models/Funcionario.ts
@@ -0,0 +1,55 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Funcionario extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public matricula: string
+
+  @column()
+  public cpf: string
+
+  @column()
+  public salario: number
+
+  @column()
+  public nome: string
+
+  @column()
+  public email: string
+
+  @column()
+  public idade: number
+
+  @column()
+  public telefone: number
+
+  @column()
+  public endereco: string
+
+  @column()
+  public numero: number
+
+  @column()
+  public bairro: string
+
+  @column()
+  public cidade: string
+
+  @column()
+  public uf: string
+
+  @column()
+  public cep: number
+
+  @column()
+  public qtdVendas: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
