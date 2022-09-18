ddfc5a7cc2d7876fbaa4cef3135d750b151c8a84
MarkMaciel <marcos.maciel2002@hotmail.com>
2022-09-18 18:10:49 -0300

    adc migration e model de veiculos

---
+++ database/migrations/1663535057205_marcas.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'marcas'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('nome', 50).notNullable()
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
