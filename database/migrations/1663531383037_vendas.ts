e237057f4aa3de5f8bee2846d4d0b39a355348a2
Rikelmelopes <rikelme.lopes777@gmail.com>
2022-09-18 17:08:49 -0300

    tabela vendas saindo do forno

---
+++ database/migrations/1663531383037_vendas.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'vendas'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('veiculo_id' ,50).unsigned().references("id").inTable("veiculos").notNullable()
+      table.integer('cliente_id').unsigned().references("id").inTable("clientes").notNullable()
+      table.integer('funcionario_id' ,1).unsigned().references("id").inTable("funcionarios").notNullable()
+      table.integer('venda_id' ,50).unsigned().references("id").inTable("vendas").notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
