94c94d4bb1af493cd91345d9ef472f7bd71772eb
MarkMaciel <marcos.maciel2002@hotmail.com>
2022-09-25 16:22:31 -0300

    atualizando tabelas

--- app/Models/Veiculo.ts
+++ app/Models/Veiculo.ts
@@ -2,8 +2,51 @@ import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Veiculo extends BaseModel {
+  
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public concessionariaId: number
+
+  @column()
+  public tipoId: number
+
+  @column()
+  public marcaId: number
+
+  @column()
+  public modelo: string
+
+  @column()
+  public ano: number
+
+  @column()
+  public preco: number
+
+  @column()
+  public estoque: string
+
+  @column()
+  public cor: string
+
+  @column()
+  public carroceria: string
+
+  @column()
+  public combustivel: string
+
+  @column()
+  public qtdPassageiros: number
+
+  @column()
+  public cambio: string
+
+  @column()
+  public cilindrada: number
+
+  @column()
+  public potencia: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
@@ -11,3 +54,5 @@ export default class Veiculo extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 }
+
+
