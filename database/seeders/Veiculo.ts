5cc02e77fad9e9ee44a966b26ee03a7e53f42d79
MarkMaciel <marcos.maciel2002@hotmail.com>
2022-09-25 16:46:43 -0300

    adc seeders de veiculos, tipo e marca

---
+++ database/seeders/Veiculo.ts
@@ -0,0 +1,23 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Veiculo from 'App/Models/Veiculo';
+
+export default class extends BaseSeeder {
+  public async run() {
+    await Veiculo.createMany([
+      {
+        concessionariaId: 1,
+        tipoId: 1,
+        marcaId: 1,
+        modelo: "Nivus",
+        ano: 2022,
+        preco: 140000.00,
+        estoque: 25,
+        cor: "Cinza Platinum",
+        combustivel: "Flex",
+        qtdPassageiros: 5,
+        cambio: "Automatico",
+        potencia: 128,
+      },
+    ]);
+  }
+}
