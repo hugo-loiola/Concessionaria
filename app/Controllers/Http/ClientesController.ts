f1021bd369592c46b85638a5c4953d1405a9411a
_digsales <diogodobu@gmail.com>
2022-10-10 19:33:01 -0300

    updating Cliente controller

--- app/Controllers/Http/ClientesController.ts
+++ app/Controllers/Http/ClientesController.ts
@@ -5,6 +5,7 @@ export default class ClientesController {
   index() {
     return Cliente.all();
   }
+
   store({ request }) {
     const dados = request.only([
       "concessionariaId",
@@ -23,5 +24,42 @@ export default class ClientesController {
       "cep",
     ]);
     return Cliente.create(dados);
+  }
+
+  show({ request }) {
+    const id = request.param("id");
+    return Cliente.findOrFail(id);
+  }
+
+  async destroy({ request }) {
+    const id = request.param("id");
+    const cliente = await Cliente.findOrFail(id);
+    return cliente.delete();
+  }
+
+  async update({ request }) {
+    const id = request.param("id");
+    const cliente = await Cliente.findOrFail(id);
+
+    const dados = request.only([
+      "concessionariaId",
+      "cpf",
+      "nome",
+      "email",
+      "dataNascimento",
+      "sexo",
+      "telefone",
+      "endereco",
+      "numero",
+      "complemento",
+      "bairro",
+      "cidade",
+      "uf",
+      "cep",
+    ]);
+
+    cliente.merge(dados).save();
+
+    return cliente;
   }
 }
