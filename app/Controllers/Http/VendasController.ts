14106ba63f275d7d0cf631167e488228ad99fe69
Rikelmelopes <rikelme.lopes777@gmail.com>
2022-10-10 19:33:49 -0300

    controllers_vendas

--- app/Controllers/Http/VendasController.ts
+++ app/Controllers/Http/VendasController.ts
@@ -15,4 +15,30 @@ export default class VendasController {
     ]);
     return Venda.create(dados);
   }
+  show({ request }) {
+    const id = request.param("id");
+    return Venda.findOrFail(id);
+  }
+
+  async destroy({ request }) {
+    const id = request.param("id");
+    const venda = await Venda.findOrFail(id);
+    return venda.delete();
+  }
+
+  async update({ request }) {
+    const id = request.param("id");
+    const venda = await Venda.findOrFail(id);
+
+    const dados = request.only([
+      "veiculoId",
+      "clienteId",
+      "funcionarioId",
+      "concessionariaId",
+    ]);
+
+    venda.merge(dados).save();
+
+    return venda;
+  }
 }
