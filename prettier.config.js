6f4beae59b12832a8f13c7c0d4a9d8fa8199db06
Hugo Loiola <hugoloiola.1@gmail.com>
2022-10-22 15:24:51 -0300

    :art:

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  semi: false,
+  singleQuote: true,
+  quoteProps: "consistent",
+  bracketSpacing: true,
+  arrowParens: "always",
+  printWidth: 100,
+  tabWidth: 2,
+};
