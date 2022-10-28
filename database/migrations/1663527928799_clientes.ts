af8470958f356c1a337c211063d9325a16128ccc
_digsales <diogodobu@gmail.com>
2022-10-28 19:53:51 -0300

    Update migrations

--- database/migrations/1663527928799_clientes.ts
+++ database/migrations/1663527928799_clientes.ts
@@ -1,40 +1,40 @@
-import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = "clientes";
+  protected tableName = 'clientes'
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments("id");
+      table.increments('id')
       table
-        .string("concessionaria_id", 50)
+        .string('concessionaria_id', 50)
         .unsigned()
-        .references("id")
-        .inTable("concessionarias")
-        .notNullable();
-      table.string("cpf", 11).notNullable();
-      table.string("nome", 100).notNullable();
-      table.string("email", 100).notNullable();
-      table.date("data_nascimento").notNullable();
-      table.string("sexo");
-      table.string("telefone", 15).notNullable();
-      table.string("endereco", 100).notNullable();
-      table.integer("numero").notNullable();
-      table.string("complemento", 100);
-      table.string("bairro", 100).notNullable();
-      table.string("cidade", 100).notNullable();
-      table.string("uf", 2).notNullable();
-      table.integer("cep").notNullable();
+        .references('id')
+        .inTable('concessionarias')
+        .notNullable()
+      table.string('cpf', 14).notNullable()
+      table.string('nome', 100).notNullable()
+      table.string('email', 100).notNullable()
+      table.date('data_nascimento').notNullable()
+      table.string('sexo', 1)
+      table.string('telefone', 15).notNullable()
+      table.string('endereco', 100).notNullable()
+      table.integer('numero').notNullable()
+      table.string('complemento', 100)
+      table.string('bairro', 100).notNullable()
+      table.string('cidade', 100).notNullable()
+      table.string('uf', 2).notNullable()
+      table.integer('cep', 9).notNullable()
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp("created_at", { useTz: true });
-      table.timestamp("updated_at", { useTz: true });
-    });
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
   }
 
   public async down() {
-    this.schema.dropTable(this.tableName);
+    this.schema.dropTable(this.tableName)
   }
 }
