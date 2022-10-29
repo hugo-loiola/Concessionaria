11a75d8dfd07e6781d9c3a5e5e6951bed7b96cde
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-10-29 00:16:44 -0300

    create funcionario validator

--- app/Controllers/Http/FuncionariosController.ts
+++ app/Controllers/Http/FuncionariosController.ts
@@ -1,71 +1,36 @@
-import Funcionario from "App/Models/Funcionario";
+import Funcionario from 'App/Models/Funcionario'
+import FuncionarioValidator from 'App/Validators/FuncionarioValidator'
 
 export default class FuncionariosController {
   // Ver todos os funcionarios
   async index() {
-    return await Funcionario.query()
-      .preload("vendas")
-      .preload("concessionaria");
+    return await Funcionario.query().preload('vendas').preload('concessionaria')
   }
   // Criar um funcionario
   async store({ request }) {
-    const dados = request.only([
-      "concessionariaId",
-      "matricula",
-      "cpf",
-      "salario",
-      "nome",
-      "email",
-      "idade",
-      "telefone",
-      "endereco",
-      "numero",
-      "complemento",
-      "bairro",
-      "cidade",
-      "uf",
-      "cep",
-      "qtdVendas",
-    ]);
-    return await Funcionario.create(dados);
+    const dados = await request.validate(FuncionarioValidator)
+    return await Funcionario.create(dados)
   }
   // Ver um funcionario em específico
   async show({ request }) {
-    const id = request.param("id");
-    return await Funcionario.findOrFail(id);
+    const id = request.param('id')
+    return await Funcionario.findOrFail(id)
   }
   // Deletar um funcionario
   async destroy({ request }) {
-    const id = request.param("id");
-    const funcionario = await Funcionario.findOrFail(id);
+    const id = request.param('id')
+    const funcionario = await Funcionario.findOrFail(id)
 
-    return await funcionario.delete();
+    return await funcionario.delete()
   }
   // Alterar um funcionario existente
   async update({ request }) {
-    const id = request.param("id");
-    const funcionario = await Funcionario.findOrFail(id);
-    const dados = request.only([
-      "concessionariaId",
-      "matricula",
-      "cpf",
-      "salario",
-      "nome",
-      "email",
-      "idade",
-      "telefone",
-      "endereco",
-      "numero",
-      "complemento",
-      "bairro",
-      "cidade",
-      "uf",
-      "cep",
-      "qtdVendas",
-    ]);
+    const id = request.param('id')
+    const funcionario = await Funcionario.findOrFail(id)
+    const dados = await request.validate(FuncionarioValidator)
 
-    funcionario.merge(dados);
+    funcionario.merge(dados)
 
-    return await funcionario.save();
+    return await funcionario.save()
   }
 }
---
+++ app/Validators/FuncionarioValidator.ts
@@ -0,0 +1,47 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class FuncionarioValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    nome: schema.string([rules.alpha({ allow: ['space'] }), rules.maxLength(50)]),
+
+    concessionaria_id: schema.number([
+      rules.unique({ table: 'concessionarias', column: 'id' }),
+      rules.exists({ table: 'concessionarias', column: 'id' }),
+    ]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages: CustomMessages = {}
+}
