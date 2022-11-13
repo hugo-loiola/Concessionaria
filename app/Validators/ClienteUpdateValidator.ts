4380339ed5db82d36f7bc6acaf01e584367b3b03
_digsales <diogodobu@gmail.com>
2022-11-13 15:36:42 -0300

    Add UpdateValidators

--- app/Validators/ClienteUpdateValidator.ts
+++ app/Validators/ClienteUpdateValidator.ts
@@ -1,40 +1,44 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, rules, CustomMessages } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ClienteUpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
-  public schema = schema.create({})
+  public schema = schema.create({
+    concessionariaId: schema.number.optional([
+      rules.exists({ table: 'concessionarias', column: 'id' }),
+    ]),
+    cpf: schema.string.optional([
+      rules.regex(/^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$/),
+      rules.unique({ table: 'clientes', column: 'cpf' }),
+    ]),
+    nome: schema.string.optional([rules.maxLength(100)]),
+    email: schema.string.optional([
+      rules.email(),
+      rules.maxLength(100),
+      rules.unique({ table: 'alunos', column: 'email' }),
+    ]),
+    dataNascimento: schema.date.optional(),
+    sexo: schema.string.optional([rules.maxLength(1), rules.alpha()]),
+    telefone: schema.string.optional([
+      rules.regex(/^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/),
+      rules.mobile({ locale: ['pt-BR'] }),
+      rules.unique({ table: 'alunos', column: 'telefone' }),
+    ]),
+    endereco: schema.string.optional([rules.maxLength(100), rules.alpha({ allow: ['space'] })]),
+    numero: schema.number.optional(),
+    complemento: schema.string.optional([rules.maxLength(100), rules.alpha({ allow: ['space'] })]),
+    bairro: schema.string.optional([
+      rules.alpha({ allow: ['space', 'dash'] }),
+      rules.maxLength(100),
+    ]),
+    cidade: schema.string.optional([
+      rules.alpha({ allow: ['space', 'dash'] }),
+      rules.maxLength(50),
+    ]),
+    uf: schema.string.optional([rules.alpha(), rules.maxLength(2)]),
+    cep: schema.string.optional([rules.regex(/[0-9]{5}-[\d]{3}/)]),
+  })
 
-  /**
-   * Custom messages for validation failures. You can make use of dot notation `(.)`
-   * for targeting nested fields and array expressions `(*)` for targeting all
-   * children of an array. For example:
-   *
-   * {
-   *   'profile.username.required': 'Username is required',
-   *   'scores.*.number': 'Define scores as valid numbers'
-   * }
-   *
-   */
   public messages: CustomMessages = {}
 }
